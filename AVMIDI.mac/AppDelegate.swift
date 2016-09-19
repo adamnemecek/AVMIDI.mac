d0ab06bd60fa9f17d608a9459d143e448c1b899b
Adam Nemecek <adamnemecek>
2016-09-18 17:00:53 -0700

    Initial Commit

---
+++ AVMIDI.mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVMIDI.mac
+//
+//  Created by Adam Nemecek on 9/18/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ AVMIDI.mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AVMIDI.mac
+//
+//  Created by Adam Nemecek on 9/18/16.
+//  Copyright © 2016 Adam Nemecek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
